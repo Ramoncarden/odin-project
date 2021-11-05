3e51421f63717087dde62efe743d6fc5ee2c20ce
Ramon Cardenas <ramoncarden@gmail.com>
2021-11-05 15:16:31 -0500

    Add solutions to project 08 calculator of the Odin project curriculum.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(x, y) {
+  if (x + y === undefined) {
+    return 0;
+  }
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  return num ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n < 2) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 };
 
 // Do not edit below this line
